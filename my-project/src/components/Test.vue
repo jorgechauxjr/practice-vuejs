d3f77c505d67afe93e9f7cc7a1fbf377616a86a2
Jorge <chauxjorge@hotmail.com>
2021-09-13 10:32:25 -0500

    Add attributes and directives v-if else for to Test.vue

--- my-project/src/components/Test.vue
+++ my-project/src/components/Test.vue
@@ -1,10 +1,44 @@
 <template>
-  <h1>Hola Mundo test</h1>
+  <div>
+    <h1 v-html="myHtml"></h1>
+    <div v-if="showName">
+      <p>{{ user.name }}</p>
+    </div>
+    <div v-else>
+      <p>No puedo mostrar nombre</p>
+    </div>
+    <hr>
+    <!-- <div>
+      {{ tasks[0].title }}
+    </div> -->
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task">
+        {{ task.title }}
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        title: 'Hello Vue',
+        user: {
+          name: 'Juan',
+          lastName: 'Perez'
+        },
+        myHtml: '<h3>Good morning Vue</h3>',
+        showName: false,
+        tasks: [
+          {title: 'eat'},
+          {title: 'dinner'},
+          {title: 'play'},
+          {title: 'sing'}
+        ]
+      }
+    }
   }
 </script>
 
