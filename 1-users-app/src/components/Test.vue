a2997e72966d198fd80c9198e8941873b4b16d00
Jorge <chauxjorge@hotmail.com>
2021-09-13 13:08:30 -0500

    Add components Test and User to use in App. 1-users-app

---
+++ 1-users-app/src/components/Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="testStyle">
+    <div>
+      {{ myMsg }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+
+    methods: {
+      
+   },
+
+  computed: {
+    
+  },
+
+// myMsg es el nombre que se le dio en el App.vue al pasar por parametro <Test myMsg="HOLA DESDE COMPONENTE PADRE App />"
+  props: {
+    myMsg: {
+      type: String,
+      default: "Mensaje por defecto cuando no pasan ninguno por parametro"
+    }
+  }
+
+  }
+</script>
+
+<style media="screen">
+  .testStyle {
+    background-color: #eee;
+  }
+</style>
---
+++ 1-users-app/src/components/User.vue
@@ -0,0 +1,43 @@
+<template lang=html>
+  <div class="users">
+    <h1>User component</h1>
+    <ul>
+      <li v-for="user in users"
+        v-bind:key="user.email">
+        {{ user.name }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: [
+          {
+            name: 'Joe',
+            email: 'joe@myemail.com',
+            contacted: false
+          },
+          {
+            name: 'Ryan',
+            email: 'ryan@myemail.com',
+            contacted: false
+          },
+          {
+            name: 'Donna',
+            email: 'donna@myemail.com',
+            contacted: true
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+  .users {
+    background: blue;
+  }
+</style>
