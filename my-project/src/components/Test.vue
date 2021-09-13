a8aa2bd4bf63ef946a2c4c3620a632fdf4455b73
Jorge <chauxjorge@hotmail.com>
2021-09-13 11:10:08 -0500

    Add directive v-model para conectar dato tipeado por usuario en Test.vue

--- my-project/src/components/Test.vue
+++ my-project/src/components/Test.vue
@@ -18,6 +18,12 @@
       </li>
     </ul>
 
+    <hr>
+    <h1>Two way data binding</h1>
+    <h2>Directiva v-model</h2>
+    <p>La directiva v-model permite linkear las cosa que puede escribir el usuario por medio de un input con las propiedades que tenemos en data
+</p>
+    <input type="text" v-model="user.name">
 
     <button type="button" 
       v-on:click="saludar(user.name)">
